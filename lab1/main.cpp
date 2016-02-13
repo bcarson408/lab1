8ece54a0fc9b20bdab9f4b92c4ee90f329d7beec
J. Brant Carson <bcarson408@gmail.com>
2016-02-13 10:24:24 -0800

    Initial Commit

---
+++ lab1/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by J. Brant Carson on 2/3/16.
+//  Copyright © 2016 J. Brant Carson. All rights reserved.
+//
+
+#include "myHeader.h"
+//#include <iostream>
+//using namespace std;
+
+int main() {
+    int i{10};
+    // insert code here...
+    cout << "Welcome to C++ !!\n"
+         <<  " Hit Enter Key to continue ...\n"
+    << i ;
+    cin.get();
+    
+}
---
+++ lab1/myHeader.h
@@ -0,0 +1,15 @@
+//
+//  myHeader.h
+//  lab1
+//
+//  Created by J. Brant Carson on 2/9/16.
+//  Copyright © 2016 J. Brant Carson. All rights reserved.
+//
+
+#ifndef myHeader_h
+#define myHeader_h
+
+#include <iostream>
+using namespace std;
+
+#endif /* myHeader_h */
